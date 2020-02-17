77eb416df8e210fcf98d0da0bc749ca205963ff5
孙常宝 <1745523163@qq.com>
2020-02-17 22:53:10 +0800

    上拉列表

--- views/home/index.vue
+++ views/home/index.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="container">
     <van-tabs v-model="activeChannelIndex">
-      <van-tab title="推荐">推荐内容展示</van-tab>
+      <van-tab title="推荐">
+        <!-- 文章列表展示区域 -->
+        <div class="scroll-wrapper">
+          <!-- 文章上拉列表 -->
+          <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+          >
+            <van-cell v-for="item in list" :key="item" :title="item" />
+          </van-list>
+        </div>
+      </van-tab>
       <van-tab title="数据库">数据库内容展示</van-tab>
       <van-tab title="后端">后端内容展示</van-tab>
     </van-tabs>
@@ -11,10 +24,10 @@
 <script>
 export default {
   name: "home-index",
-  data(){
-      return{
-          activeChannelIndex:2
-      }
+  data() {
+    return {
+      activeChannelIndex: 2
+    };
   }
 };
 </script>
