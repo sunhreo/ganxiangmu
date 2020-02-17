c6b6f8f4f4822e3f9d70c3d66f49c97a55f96cb4
孙常宝 <1745523163@qq.com>
2020-02-17 21:56:55 +0800

    vuex管理token状态

--- store/index.js
+++ store/index.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+const USER_KEY = 'hm-toutiao-m-user'
 export default new Vuex.Store({
   state: {
+    user: JSON.parse(localStorage.getItem(USER_KEY) || '{}')
   },
   mutations: {
+    undateUser (state, data) {
+      state.user = data
+      localStorage.setItem(USER_KEY,JSON.stringify(data))
+    },
+    clearUser (state) {
+      // 1.vuex做清除,使得有响应式
+      state.user = {}
+      // 2.localstorage做持久清除
+      localStorage.removeItem(USER_KEY)
+    }
   },
   actions: {
   },
