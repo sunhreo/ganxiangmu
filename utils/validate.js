7ba946e676f13488f428f9629f366a461464e1de
孙常宝 <1745523163@qq.com>
2020-02-17 22:45:01 +0800

    中英文切换

--- utils/validate.js
+++ utils/validate.js
@@ -1,7 +1,20 @@
-// 导入默认校验规则
-import { extend } from 'vee-validate'
+// 1. 导入localize模块函数
+import { extend, localize } from 'vee-validate'
+// 导入全部的规则
 import * as rules from 'vee-validate/dist/rules'
-// rules: {alpha:xx,alpha_dash:xx,alpha_num:xx……}
+
+// 2.导入要使用的语言
+import zhCN from 'vee-validate/dist/locale/zh_CN.json'
+
+// 3.对导入好的语言做注册
+localize({
+  // 对象成员简易赋值
+  zhCN
+})
+// 4. 使用具体的语言
+localize('zhCN')
+
+// 遍历、注册全部的规则
 Object.keys(rules).forEach(rule => {
   extend(rule, rules[rule])
 })
