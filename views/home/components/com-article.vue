592f6ef8bd6b5f167bdc34d08edc0eee8822b17f
孙常宝 <1745523163@qq.com>
2020-02-17 23:08:50 +0800

    ’“获取文章实操

--- views/home/components/com-article.vue
+++ views/home/components/com-article.vue
@@ -10,16 +10,25 @@
         finished-text="没有更多了"
         @load="onLoad"
       >
-        <van-cell v-for="item in list" :key="item" :title="item" />
+        <van-cell
+          v-for="item in articleList"
+          :key="item.art_id.toString()"
+          :title="item.title"
+        />
       </van-list>
     </van-pull-refresh>
   </div>
 </template>
 <script>
+import { apiArticleList } from "@/api/article";
 export default {
   name: "com-article",
   data() {
     return {
+      // 声明一个时间戳成员，用于获取文章使用
+      ts: Date.now(),
+      // 当前频道文章列表信息
+      articleList: [],
       isLoading: false,
       list: [],
       loading: false,
@@ -34,6 +43,18 @@ export default {
     }
   },
   methods: {
+    // 获得文章列表
+    async getArticleList() {
+      let result = await apiArticleList({
+        channel_id: this.channel_id,
+        timestamp: this.ts,
+        with_top: 1
+      });
+      // data接收获取的文章
+      this.articleList = result.results;
+      // 更新时间戳信息
+      this.ts = result.pre_timestamp;
+    },
     // 下拉刷新载入
     onRefresh() {
       setTimeout(() => {
@@ -48,14 +69,9 @@ export default {
         for (let i = 0; i < 10; i++) {
           this.list.push(this.list.length + 1);
         }
-        // 加载状态结束
-        this.loading = false;
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true;
-        }
-      }, 500);
+        // 获取文章
+        this.getArticleList();
+      });
     }
   }
 };
