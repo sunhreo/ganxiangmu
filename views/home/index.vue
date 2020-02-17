b724ff2380484e472dce316ab9d47a0180711997
孙常宝 <1745523163@qq.com>
2020-02-17 22:52:34 +0800

    频道展示

--- views/home/index.vue
+++ views/home/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="container">Home</div>
+  <div class="container">
+    <van-tabs v-model="activeChannelIndex">
+      <van-tab title="推荐">推荐内容展示</van-tab>
+      <van-tab title="数据库">数据库内容展示</van-tab>
+      <van-tab title="后端">后端内容展示</van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "home-index"
+  name: "home-index",
+  data(){
+      return{
+          activeChannelIndex:2
+      }
+  }
 };
 </script>
 
--- views/layout/index.vue
+++ views/layout/index.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="container">
-    <van-nav-bar
-      fixed
-      title="黑马头条"
-      right-text="搜索"
-      @click-right="$router.push('/search')"
-    />
-    <div class="my-wrapper">
+    <!--头部导航-->
+    <van-nav-bar fixed title="黑马头条" right-text="搜索"
+      @click-right="$router.push('/search')"/>
+    <div class="my-wrapper" >
+      <!--路由占位符-->
       <router-view></router-view>
     </div>
+    <!--底部导航-->
     <van-tabbar route>
-      <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
-      <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
-      <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/question"  icon="chat-o">问答</van-tabbar-item>
+      <van-tabbar-item to="/video"  icon="video-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user"  icon="user-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
